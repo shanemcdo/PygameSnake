12ff1157ac7e4d1c19973263f870fccca52e5557
Shane McDonough <shanemcdman@gmail.com>
2021-01-12 17:52:09 -0800

    Make window_size and real_window_size use Point

--- pygame_tools.py
+++ pygame_tools.py
@@ -288,9 +288,9 @@ class GameScreen:
         self.window_scaled = bool(window_size) and window_size != real_window_size
         self.real_screen = screen
         self.screen = screen if not self.window_scaled else pygame.Surface(window_size)
-        self.real_window_size = real_window_size
-        self.window_size = window_size if self.window_scaled else real_window_size
-        self.window_scale = Point(real_window_size.x // window_size.x, real_window_size.y // window_size.y)
+        self.real_window_size = Point._make(real_window_size)
+        self.window_size = Point._make(window_size if self.window_scaled else real_window_size)
+        self.window_scale = Point(self.real_window_size.x // self.window_size.x, self.real_window_size.y // self.window_size.y)
         self.frame_rate = frame_rate
         self.running = False
         self.rect = self.screen.get_rect()
